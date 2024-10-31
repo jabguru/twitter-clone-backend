a4032d09227503a74911a6f8b89916d96d0a67ec
Julius Alibrown <brownjulius980@gmail.com>
2024-10-31 15:09:27 +0100

    worked on upload profile images

--- src/main/java/com/jabguru/TwitterClone/user/UserController.java
+++ src/main/java/com/jabguru/TwitterClone/user/UserController.java
@@ -1,20 +1,48 @@
 package com.jabguru.TwitterClone.user;
 
+import com.jabguru.TwitterClone.image.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final ImageService imageService;
 
     @PostMapping("/save")
-    public ResponseEntity<String> saveUser(@RequestBody User user){
+    public ResponseEntity<String> saveUser(@ModelAttribute User user, @RequestParam("profilePhoto") Optional<MultipartFile> profilePhoto, @RequestParam("bannerPhoto") Optional<MultipartFile> bannerPhoto){
+        if(profilePhoto.isPresent()){
+            String url;
+            try {
+                url = imageService.saveImage(profilePhoto.get());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            user.setProfilePic(url);
+        }
+
+        if(bannerPhoto.isPresent()){
+            String url;
+            try {
+                url = imageService.saveImage(bannerPhoto.get());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            user.setBannerPic(url);
+        }
+
         userService.saveUser(user);
         return new ResponseEntity<>("User saved successfully", HttpStatus.OK);
     }
