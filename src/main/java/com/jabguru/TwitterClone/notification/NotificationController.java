de48458a6906e12962bfcf4a6235901f87644a91
Julius Alibrown <brownjulius980@gmail.com>
2024-10-31 17:08:41 +0100

    updated create notification

--- src/main/java/com/jabguru/TwitterClone/notification/NotificationController.java
+++ src/main/java/com/jabguru/TwitterClone/notification/NotificationController.java
@@ -1,5 +1,7 @@
 package com.jabguru.TwitterClone.notification;
 
+import com.jabguru.TwitterClone.user.User;
+import com.jabguru.TwitterClone.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,21 @@ import java.util.List;
 @RequestMapping("api/v1/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
+    private final UserService userService;
+
 
     @PostMapping("/create")
-    public ResponseEntity<String> createNotification(@RequestBody Notification notification){
-        notificationService.createNotification(notification);
-        return new ResponseEntity<>("Notification created successfully", HttpStatus.CREATED);
+    public ResponseEntity<String> createNotification(@RequestBody Notification notification, @RequestParam int userId){
+        User user = userService.getUser(userId);
+
+        if(user != null){
+            notification.setUser(user);
+            notificationService.createNotification(notification);
+            return new ResponseEntity<>("Notification created successfully", HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
     @GetMapping("/user/{userId}")
